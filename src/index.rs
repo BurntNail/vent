21153347b4a18ed7dad8f8a7ccfc2994352a2e2d
BurntNail <github.29x6f@simplelogin.com>
2023-05-04 21:28:42 +0000

    insert elements into db

--- src/index.rs
+++ src/index.rs
@@ -1,21 +1,24 @@
-use std::env::current_dir;
-
-use axum::{response::{Html}, Form};
-use chrono::{TimeZone, Local};
+use std::{env::current_dir, sync::Arc};
+use sqlx::types::chrono::{NaiveDateTime};
+use axum::{extract::State, response::Html, Form};
 use liquid::ParserBuilder;
-use serde::{Deserialize};
+use serde::{Deserialize, Serialize};
+use sqlx::{Pool, Postgres};
 use tokio::fs::read_to_string;
 
-pub async fn root () -> Html<String> {
+pub async fn root(State(pool): State<Arc<Pool<Postgres>>>) -> Html<String> {
     let liquid = read_to_string("www/templates/index.liquid").await.unwrap();
     let template = ParserBuilder::with_stdlib()
-        .build().unwrap()
-        .parse(&liquid).unwrap();
+        .build()
+        .unwrap()
+        .parse(&liquid)
+        .unwrap();
 
-    let cd = current_dir().map(|cd| cd.to_str().map(|x| x.to_string())).unwrap_or(Some("failed to get cd".into())).unwrap();
-    let globals = liquid::object!({
-        "cd": cd
-    });
+    let cd = current_dir()
+        .map(|cd| cd.to_str().map(|x| x.to_string()))
+        .unwrap_or(Some("failed to get cd".into()))
+        .unwrap();
+    let globals = liquid::object!({ "cd": cd });
 
     let output = template.render(&globals).unwrap();
 
@@ -23,19 +26,55 @@ pub async fn root () -> Html<String> {
 }
 
 #[derive(Deserialize, Debug)]
-pub struct Event {
-    name: String,
-    date: String,
-    location: String,
-    teacher: String,
-    prefects: String,
-    info: String,
+pub struct HtmlEvent {
+    pub name: String,
+    pub date: String,
+    pub location: String,
+    pub teacher: String,
+    pub prefects: String,
+    pub info: String,
 }
 
-pub async fn root_form (Form(Event {name, date, location, teacher, prefects, info}): Form<Event>) -> Html<String> {
-    let date = Local::now().timezone().datetime_from_str(&date, "%Y-%m-%dT%H:%M").expect("error getting date/time");
+pub struct DbEvent {
+    pub name: String,
+    pub date: NaiveDateTime,
+    pub location: String,
+    pub teacher: String,
+    pub prefects: String,
+    pub info: String,
+}
 
-    info!(?name, ?date, ?location, ?teacher, ?prefects, ?info, "Got form response");
+impl TryFrom<HtmlEvent> for DbEvent {
+    type Error = ();
+
+    fn try_from(HtmlEvent {name, date, location, teacher, prefects, info}: HtmlEvent) -> Result<Self, Self::Error> {
+        let date = NaiveDateTime::parse_from_str(&date, "%Y-%m-%dT%H:%M").map_err(|_e| ())?; //we love not exposing data types lol
+
+        Ok(Self {
+            name, date, location, teacher, prefects, info
+        })
+    }
+}
+
+pub async fn root_form(
+    State(pool): State<Arc<Pool<Postgres>>>,
+    Form(event): Form<HtmlEvent>,
+) -> Html<String> {
+    let DbEvent { name, date, location, teacher, prefects, info } = DbEvent::try_from(event).expect("error parsing html event");
+
+    let mut conn = pool.acquire().await.expect("error getting database connection");
+
+    sqlx::query!(
+        "INSERT INTO events (name, date, location, teacher, prefects, participants, other_info) 
+        VALUES ($1, $2, $3, $4, $5, $6, $7)",
+        name,
+        date,
+        location,
+        teacher,
+        prefects,
+        "".into(),
+        info
+    ).execute(&mut conn).await.expect("unable to add event");
 
     root().await
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 mod index;
 
-use std::net::SocketAddr;
+use std::{net::SocketAddr, sync::Arc};
 use sqlx::postgres::PgPoolOptions;
 use axum::{routing::get, Router};
 use index::{root, root_form};
@@ -12,10 +12,10 @@ async fn main() {
     dotenvy::dotenv().expect("unable to get env variables");
     tracing_subscriber::fmt::init();
 
-    let app = Router::new().route("/", get(root).post(root_form));
-
     let db_url = std::env::var("DATABASE_URL").expect("DB URL must be set");
-    let pool = PgPoolOptions::new().max_connections(5).connect(&db_url).await.expect("cannot connect to DB");
+    let pool = Arc::new(PgPoolOptions::new().max_connections(5).connect(&db_url).await.expect("cannot connect to DB"));
+
+    let app = Router::new().route("/", get(root).post(root_form)).with_state(pool);
 
     axum::Server::bind(&SocketAddr::from(([127, 0, 0, 1], 8080)))
         .serve(app.into_make_service())
