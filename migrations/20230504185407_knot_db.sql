6a7553df190e168f7d6f8649c3b6c858f1d8378f
BurntNail <github.29x6f@simplelogin.com>
2023-05-10 17:28:45 +0000

    mvp for multiple tables

--- migrations/20230504185407_knot_db.sql
+++ migrations/20230504185407_knot_db.sql
@@ -1,9 +1,37 @@
-CREATE TABLE IF NOT EXISTS events (
-    name TEXT NOT NULL,
+CREATE TABLE events (
+    id SERIAL PRIMARY KEY,
+    event_name TEXT NOT NULL,
     date TIMESTAMP NOT NULL,
     location TEXT NOT NULL,
     teacher TEXT NOT NULL,
-    prefects TEXT NOT NULL,
-    participants TEXT NOT NULL,
     other_info TEXT
-)
+);
+
+CREATE TABLE people (
+    id SERIAL PRIMARY KEY,
+    person_name TEXT NOT NULL
+);
+
+CREATE TABLE prefect_events (
+    prefect_id INT NOT NULL,
+    CONSTRAINT fk_prefect_id 
+        FOREIGN KEY (prefect_id)
+        REFERENCES people(id),
+
+    event_id INT NOT NULL,
+    CONSTRAINT fk_event_id
+        FOREIGN KEY (event_id)
+        REFERENCES events(id)
+);
+
+CREATE TABLE participant_events (
+    participant_id INT NOT NULL,
+    CONSTRAINT fk_participant_id 
+        FOREIGN KEY (participant_id)
+        REFERENCES people(id),
+
+    event_id INT NOT NULL,
+    CONSTRAINT fk_event_id
+        FOREIGN KEY (event_id)
+        REFERENCES events(id)
+);
--- src/index.rs
+++ src/index.rs
@@ -11,12 +11,19 @@ pub async fn root(State(pool): State<Arc<Pool<Postgres>>>) -> Html<String> {
 
     #[derive(Serialize)]
     struct Anon {
-        pub name: String
+        pub event_name: String,
+        pub person_name: String,
     }
+
 
     let events = sqlx::query_as!(
         Anon,
-        "SELECT name FROM events"
+        r#"
+        SELECT e.event_name, p.person_name 
+        FROM events e
+        INNER JOIN participant_events pe ON pe.event_id = e.id
+        INNER JOIN people p ON p.id = pe.participant_id
+        "#
     ).fetch_all(&mut conn).await.expect("error getting stuff");
 
     //MVP to see event names as can't serialise the date time.
@@ -33,7 +40,7 @@ pub async fn root(State(pool): State<Arc<Pool<Postgres>>>) -> Html<String> {
 
     let output = template.render(&globals).expect("error rendering");
 
-    Html(output.to_string())
+    Html(output)
 }
 
 #[derive(Deserialize, Debug)]
@@ -42,7 +49,6 @@ pub struct HtmlEvent {
     pub date: String,
     pub location: String,
     pub teacher: String,
-    pub prefects: String,
     pub info: String,
 }
 
@@ -51,19 +57,17 @@ pub struct DbEvent {
     pub date: NaiveDateTime,
     pub location: String,
     pub teacher: String,
-    pub prefects: String,
-    pub participants: String,
     pub other_info: String,
 }
 
 impl TryFrom<HtmlEvent> for DbEvent {
     type Error = ();
 
-    fn try_from(HtmlEvent {name, date, location, teacher, prefects, info}: HtmlEvent) -> Result<Self, Self::Error> {
+    fn try_from(HtmlEvent {name, date, location, teacher, info}: HtmlEvent) -> Result<Self, Self::Error> {
         let date = NaiveDateTime::parse_from_str(&date, "%Y-%m-%dT%H:%M").map_err(|_e| ())?; //we love not exposing data types lol
 
         Ok(Self {
-            name, date, location, teacher, prefects, other_info: info, participants: String::new()
+            name, date, location, teacher, other_info: info
         })
     }
 }
@@ -72,19 +76,17 @@ pub async fn root_form(
     State(pool): State<Arc<Pool<Postgres>>>,
     Form(event): Form<HtmlEvent>,
 ) -> Html<String> {
-    let DbEvent { name, date, location, teacher, prefects, other_info, participants } = DbEvent::try_from(event).expect("error parsing html event");
+    let DbEvent { name, date, location, teacher, other_info } = DbEvent::try_from(event).expect("error parsing html event");
 
     let mut conn = pool.acquire().await.expect("error getting database connection");
 
     sqlx::query!(
-        "INSERT INTO events (name, date, location, teacher, prefects, participants, other_info) 
-        VALUES ($1, $2, $3, $4, $5, $6, $7)",
+        "INSERT INTO events (event_name, date, location, teacher, other_info) 
+        VALUES ($1, $2, $3, $4, $5)",
         name,
         date,
         location,
         teacher,
-        prefects,
-        participants,
         other_info
     ).execute(&mut conn).await.expect("unable to add event");
 
