33d95817e7b8cdea61d81fbbd99cafa7aab9e256
Jack Maguire/BurntNail <github.29x6f@simplelogin.com>
2023-10-28 13:35:41 +0000

    fix weird path for edit_self.liquid

--- src/routes/edit_self.rs
+++ src/routes/edit_self.rs
@@ -17,7 +17,7 @@ pub async fn get_edit_user(
     State(state): State<KnotState>,
 ) -> Result<impl IntoResponse, KnotError> {
     compile_with_newtitle(
-        "../../www/edit_self.liquid",
+        "www/edit_self.liquid",
         liquid::object!({"auth": get_auth_object(auth)}),
         &state.settings.brand.instance_name,
         Some("Edit Profile".into()),
